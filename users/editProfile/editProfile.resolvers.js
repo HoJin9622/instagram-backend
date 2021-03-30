1b2fff9e4b3612245046033dca9ab7bf17448392
HoJin9622 <kiss0104040@gmail.com>
2021-03-30 20:22:13 +0900

    4.9 authentication part Two

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -6,16 +6,16 @@ export default {
   Mutation: {
     editProfile: async (
       _,
-      { firstName, lastName, username, email, password: newPassword, token }
+      { firstName, lastName, username, email, password: newPassword },
+      { loggedInUser }
     ) => {
-      const { id } = await jwt.verify(token, process.env.SECRET_KEY)
-
+      console.log(loggedInUser)
       let uglyPassword = null
       if (newPassword) {
         uglyPassword = await bcrypt.hash(newPassword, 10)
       }
       const ok = await client.user.update({
-        where: { id },
+        where: { id: loggedInUser.id },
         data: {
           firstName,
           lastName,
