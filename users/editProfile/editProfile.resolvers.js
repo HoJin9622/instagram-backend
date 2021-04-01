f5d2896f3b7cefd50fc9b98979cb8bf63b62d2b8
HoJin9622 <kiss0104040@gmail.com>
2021-04-01 22:09:42 +0900

    Protecting Resolvers part Two

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,38 +1,40 @@
 import bcrypt from 'bcrypt'
 import client from '../../client'
+import { protectedResolver } from '../users.utils'
 
 export default {
   Mutation: {
-    editProfile: async (
-      _,
-      { firstName, lastName, username, email, password: newPassword },
-      { loggedInUser, protectResolver }
-    ) => {
-      protectResolver(loggedInUser)
-      let uglyPassword = null
-      if (newPassword) {
-        uglyPassword = await bcrypt.hash(newPassword, 10)
-      }
-      const ok = await client.user.update({
-        where: { id: loggedInUser.id },
-        data: {
-          firstName,
-          lastName,
-          username,
-          email,
-          ...(uglyPassword && { password: uglyPassword }),
-        },
-      })
-      if (ok) {
-        return {
-          ok: true,
+    editProfile: protectedResolver(
+      async (
+        _,
+        { firstName, lastName, username, email, password: newPassword },
+        { loggedInUser }
+      ) => {
+        let uglyPassword = null
+        if (newPassword) {
+          uglyPassword = await bcrypt.hash(newPassword, 10)
         }
-      } else {
-        return {
-          ok: false,
-          error: 'Could not update profile.',
+        const ok = await client.user.update({
+          where: { id: loggedInUser.id },
+          data: {
+            firstName,
+            lastName,
+            username,
+            email,
+            ...(uglyPassword && { password: uglyPassword }),
+          },
+        })
+        if (ok) {
+          return {
+            ok: true,
+          }
+        } else {
+          return {
+            ok: false,
+            error: 'Could not update profile.',
+          }
         }
       }
-    },
+    ),
   },
 }
--- users/users.utils.js
+++ users/users.utils.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken'
 import client from '../client'
 
-export const getUser = async token => {
+export const getUser = async (token) => {
   try {
     if (!token) {
       return null
@@ -18,8 +18,17 @@ export const getUser = async token => {
   }
 }
 
-export const protectResolver = user => {
-  if (!user) {
-    throw new Error('You need to login.')
+export const protectedResolver = (ourResolver) => (
+  root,
+  args,
+  context,
+  info
+) => {
+  if (!context.loggedInUser) {
+    return {
+      ok: false,
+      error: 'Please log in to perform this action.',
+    }
   }
+  return ourResolver(root, args, context, info)
 }
