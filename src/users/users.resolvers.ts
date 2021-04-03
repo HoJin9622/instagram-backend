8e688e425ea77c9d617215d498609d60c1dd9143
HoJin9622 <kiss0104040@gmail.com>
2021-04-03 21:38:42 +0900

    4.28 Computed Fields part Three

--- src/users/users.resolvers.ts
+++ src/users/users.resolvers.ts
@@ -24,6 +24,20 @@ const resolvers: Resolvers = {
       }
       return id === loggedInUser.id
     },
+    isFollowing: async ({ id }, _, { loggedInUser, client }) => {
+      if (!loggedInUser) {
+        return false
+      }
+      const exists = await client.user.count({
+        where: {
+          username: loggedInUser.username,
+          following: {
+            some: { id },
+          },
+        },
+      })
+      return Boolean(exists)
+    },
   },
 }
 
