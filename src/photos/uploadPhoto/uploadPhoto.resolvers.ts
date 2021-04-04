f313689887b24f2efc1195a86231206458ca6890
HoJin9622 <kiss0104040@gmail.com>
2021-04-05 00:06:59 +0900

    6.21 S3 Photo Upload part Three

--- src/photos/uploadPhoto/uploadPhoto.resolvers.ts
+++ src/photos/uploadPhoto/uploadPhoto.resolvers.ts
@@ -1,3 +1,4 @@
+import { uploadToS3 } from '../../shared/shared.utils'
 import { Resolvers } from '../../types'
 import { protectedResolver } from '../../users/users.utils'
 import { processHashtags } from '../photos.utils'
@@ -10,9 +11,10 @@ const resolvers: Resolvers = {
         if (caption) {
           hashtagObjs = processHashtags(caption)
         }
+        const fileUrl = await uploadToS3(file, loggedInUser.id, 'uploads')
         return client.photo.create({
           data: {
-            file,
+            file: fileUrl,
             caption,
             user: { connect: { id: loggedInUser.id } },
             ...(hashtagObjs.length > 0 && {
