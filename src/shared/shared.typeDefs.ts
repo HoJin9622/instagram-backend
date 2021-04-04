42e854d2cea77be4ffa385a10342dcc6e2c4f09d
HoJin9622 <kiss0104040@gmail.com>
2021-04-04 23:26:02 +0900

    6.17 editComment

--- src/shared/shared.typeDefs.ts
+++ src/shared/shared.typeDefs.ts
@@ -1,7 +1,8 @@
 import { gql } from 'apollo-server-core'
 
 export default gql`
-  type Mutation {
-    toogleLike(id: Int!): MutationResponse!
+  type MutationResponse {
+    ok: Boolean!
+    error: String
   }
 `
