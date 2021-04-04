514c6507a7d9c6ef03472035495f6be4becd8cad
HoJin9622 <kiss0104040@gmail.com>
2021-04-04 15:40:47 +0900

    6.5 seePhoto

---
+++ src/photos/photos.resolvers.ts
@@ -0,0 +1,13 @@
+import { Resolvers } from '../types'
+
+const resolvers: Resolvers = {
+  Photo: {
+    user: ({ userId }, _, { client }) => {
+      return client.user.findUnique({ where: { id: userId } })
+    },
+    hashtags: ({ id }, _, { client }) =>
+      client.hashtag.findMany({ where: { photos: { some: { id } } } }),
+  },
+}
+
+export default resolvers
